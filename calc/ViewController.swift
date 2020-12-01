a8962dd3dc9a060f7e3e8a60e30887c42032b7d8
DushaRaz <cool.pivovarov2012@yandex.ru>
2020-12-01 10:31:11 +0300

    Чуть переделал интерфейс и добавил анимацию для resultLabel Насколько я понимаю, анимацию вставлять больше не куда

--- calc/ViewController.swift
+++ calc/ViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super .viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        resultLabel.center.x -= view.bounds.width
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0){
+            self.resultLabel.center.x += self.view.bounds.width
+        }
+
     }
 
+    var operand = ""
+    var numberOne = ""
+    var numberTwo = ""
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func inputNumber(_ sender: UIButton) {
+        
+        if operand.isEmpty{
+            numberOne = numberOne + (sender.titleLabel?.text)!
+            resultLabel.text = numberOne
+        } else {
+            numberTwo = numberTwo + (sender.titleLabel?.text)!
+            resultLabel.text = numberTwo
+        }
+        
+    }
+    
+    @IBAction func clearAction(_ sender: UIButton) {
+        numberOne = ""
+        numberTwo = ""
+        operand = ""
+        resultLabel.text = "0"
+    }
+    
+    @IBAction func inputOperand(_ sender: UIButton) {
+        operand = sender.titleLabel?.text as! String
+    }
+    
+    @IBAction func resultAction(_ sender: UIButton) {
+        var result = 0.0
+        
+        switch  operand {
+        case "/":
+            result = Double(numberOne)! / Double(numberTwo)!
+        case "*":
+            result = Double(numberOne)! * Double(numberTwo)!
+        case "+":
+            result = Double(numberOne)! + Double(numberTwo)!
+        case "-":
+            result = Double(numberOne)! - Double(numberTwo)!
+        default:
+            break
+        }
+        
+        if result.truncatingRemainder(dividingBy: 1.0) == 00 {
+            resultLabel.text = String(Int(result))
+        } else {
+            resultLabel.text = String(result) 
+        }
+        
+        numberOne = String(Int(result))
+        operand = ""
+        numberTwo = ""
+
+    }
+    
 }
 
